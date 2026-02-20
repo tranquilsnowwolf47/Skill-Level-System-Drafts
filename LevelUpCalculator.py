de4c59dc4e01f37d2e7f6c0239c2e57f85372850
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-20 15:46:20 -0500

    Create LevelUpCalculator.py

---
+++ LevelUpCalculator.py
@@ -0,0 +1,502 @@
+# Filename:
+# Date: 1/15/25
+# Author: Akira | oathkeeperthefoolxiii
+
+
+# Processing: 
+# Functions used 
+
+
+# This programs calculates the level ups of the user dependent on the EXP value that they give. Using a loop, they can loop as many times as they want until they get to the level that exhausts
+# the value they entered. Using for final letter calculations
+#
+
+
+# E+
+def to_E_Plus(TO_E_PLUS_EXP):
+    #TO_E_PLUS_EXP = 40
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_E_PLUS_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_E_PLUS_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_E_PLUS_EXP}")
+
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+        #    loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_E_Plus(40)
+
+
+# D
+def to_D(TO_D_EXP):
+    #TO_D_EXP = 60
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_D_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_D_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_D_EXP}")
+
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+        #    loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_D(60)
+
+
+# D+
+def to_D_Plus(TO_D_PLUS_EXP):
+    #TO_D_PLUS_EXP = 80
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_D_PLUS_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_D_PLUS_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_D_PLUS_EXP}")
+
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+        #    loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_D_Plus(80)
+
+# C
+def to_C(TO_C_EXP):
+    #TO_C_EXP = 120
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_C_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_C_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_C_EXP}")
+
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+        #    loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_C(120)
+
+
+# C+
+def to_C_Plus(TO_C_PLUS_EXP):
+    #TO_C_PLUS_EXP = 160
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_C_PLUS_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_C_PLUS_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_C_PLUS_EXP}")
+
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+        #    loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_C_Plus(160)
+
+
+# B
+def to_B(TO_B_EXP):
+    #TO_B_EXP = 220
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_B_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_B_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_B_EXP}")
+
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+        #    loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_B(220)
+
+
+# B+
+def to_B_Plus(TO_B_PLUS_EXP):
+    #TO_B_PLUS_EXP = 280
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_B_PLUS_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_B_PLUS_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_B_PLUS_EXP}")
+
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+         #   loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_B_Plus(280)
+
+# A
+def to_A(TO_A_EXP):
+    #TO_A_EXP = 360
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_A_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_A_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_A_EXP}")
+
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+        #    loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_A(360)
+
+
+# A+
+def to_A_Plus(TO_A_PLUS_EXP):
+    #TO_A_PLUS_EXP = 440
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_A_PLUS_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_A_PLUS_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_A_PLUS_EXP}")
+            
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+        #    loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_A_Plus(440)
+
+
+# S
+def to_S(TO_S_EXP):
+    #TO_S_EXP = 540
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_S_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_S_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_S_EXP}")
+
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+        #    loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_S(540)
+
+
+# S+
+def to_S_Plus(TO_S_PLUS_EXP):
+    #TO_S_PLUS_EXP = 760
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_S_PLUS_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_S_PLUS_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_S_PLUS_EXP}")
+
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+        #    loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_S_Plus(760)
+
+
+# Max
+def to_MAX(TO_MAX_EXP):
+    # TO_MAX_EXP = 900
+    #loop = True # Value for the loop
+    levelUp = False # If a level up was obtained
+
+    #while loop:
+    EXPInput = int(input("Please enter the exp to next: "))
+    if EXPInput >= TO_MAX_EXP:
+        levelUp = True
+        hasRemainder = True
+        remainder = EXPInput - TO_MAX_EXP
+        print()
+        print("You leveled up!")
+        #print(f"hasRemainder: {hasRemainder}") # Debugging
+        print(f"Remainder: {remainder} EXP")
+    else:
+        levelUp = False
+        print("You didn't level up...")
+        print(f"The number of EXP entered is: {EXPInput}")
+        print(f"The value of the next letter is: {TO_MAX_EXP}")
+
+        print()
+        #loopPrompt = str(input("Would you like to try again? Enter y to do so. "))
+        #if loopPrompt == 'y':
+        #    loop
+        #else:
+        #    loop == False
+    
+
+# Call function
+#to_MAX(900)
+
+
+
+
+
+
+# Main operations
+# ----------------------------------------------------------------------
+
+# Default value 
+validLetterChoice = False
+
+letterLoop = True # Value for the loop
+while letterLoop:
+    letterChoice = input("Please enter the letter of the next value or enter MAX for max: ")
+    Letters = ["E+","D","D+","C","C+","B","B+","A","A+","S","S+","MAX"]
+
+    
+    SkillCategories = ["Sword","Lance","Axe","Bow","Brawl","Reason","Faith","Authority","Hvy Armor","Riding","Flying"]
+    
+
+    if letterChoice in Letters:
+        validLetterChoice = True
+        #print(f"validLetterChoice: {validLetterChoice}")    # Debugging
+        if validLetterChoice == True:
+            if letterChoice == "E+":
+                to_E_Plus(40)
+                print(f"New current letter: {letterChoice}")
+            elif letterChoice == "D":
+                to_D(60)
+                print(f"New current letter: {letterChoice}")
+            elif letterChoice == "D+":
+                to_D_Plus(80)
+                print(f"New current letter: {letterChoice}")
+            elif letterChoice == "C":
+                to_C(120)
+                print(f"New current letter: {letterChoice}")
+            elif letterChoice == "C+":
+                to_C_Plus(160)
+                print(f"New current letter: {letterChoice}")
+            elif letterChoice == "B":
+                to_B(220)
+                print(f"New current letter: {letterChoice}")
+            elif letterChoice == "B+":
+                to_B_Plus(280)
+                print(f"New current letter: {letterChoice}")
+            elif letterChoice == "A":
+                to_A(360)
+                print(f"New current letter: {letterChoice}")
+            elif letterChoice == "A+":
+                to_A_Plus(440)
+                print(f"New current letter: {letterChoice}")
+            elif letterChoice == "S":
+                to_S(540)
+                print(f"New current letter: {letterChoice}")
+            elif letterChoice == "S+":
+                to_S_Plus(760)
+                print(f"New current letter: {letterChoice}")
+            elif letterChoice == "MAX":
+                to_MAX(900)
+                print(f"New current letter: {letterChoice}")
+                # Reprompt if the user wants to do the loop again        
+                letterLoopPrompt = str(input("Would you like to do another? Enter y for yes: "))
+                if letterLoopPrompt == "y":
+                    letterLoop
+                else:
+                    letterLoop == False   
+    else:    # Cleared
+        #print(f"validLetterChoice: {validLetterChoice}")    # Debugging
+        print("You did not enter the correct letter...")
+        letterLoop = False # Exit the loop 
+        print()
+        letterLoopPrompt = str(input("Would you like to try again? Enter y to continue or enter any other character to exit: ")) # Ask if they'd like to continue the loop
+        print()
+        if letterLoopPrompt == "y":
+            letterLoop = True
+        else:
+            letterLoop = False
+            print()
+            print("You have exited the program. Goodbye!")
+
+    
+
+        
