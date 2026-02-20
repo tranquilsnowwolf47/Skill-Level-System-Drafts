58b2791bd0b57c687f84c8e645e9848a450d4bde
Aoi <spadesxoathkeeper17@gmail.com>
2026-02-20 15:47:07 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,7 @@
+date = input("Please enter the date: (MM-DD-YY)": )
+
+stats = {"Sword": 0, "Lance": 0, "Axe": 0,
+    "Bow": 0, "Brawl": 0, "Reason": 0,
+    "Faith": 0, "Authority": 0, 
+    "Heavy Armor":0, "Riding":0, 
+    "Flying": 0)
